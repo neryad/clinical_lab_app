168baf87a09e9b5fbd804c54f2030d90d6df53df
neryad <dayern.gomez@hotmail.com>
2025-12-25 15:15:21 -0400

    feat: Add Netlify build configuration and Supabase environment variable constants.

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,6 +1,12 @@
 class SupabaseConstants {
   // TODO: Replace with your actual Supabase URL and Anon Key
-  static const String url = 'https://dzwvrgnxwcfwhinqzwlc.supabase.co';
-  static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6d3ZyZ254d2Nmd2hpbnF6d2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDcyMTUsImV4cCI6MjA3OTY4MzIxNX0.bQlskVaTvHgIgPmk0he8_IeO41INJQ_-qpQOSrJjVPM';
+  static const String url = String.fromEnvironment(
+    'SUPABASE_URL',
+    defaultValue: 'https://dzwvrgnxwcfwhinqzwlc.supabase.co',
+  );
+  static const String anonKey = String.fromEnvironment(
+    'SUPABASE_ANON_KEY',
+    defaultValue:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6d3ZyZ254d2Nmd2hpbnF6d2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDcyMTUsImV4cCI6MjA3OTY4MzIxNX0.bQlskVaTvHgIgPmk0he8_IeO41INJQ_-qpQOSrJjVPM',
+  );
 }
